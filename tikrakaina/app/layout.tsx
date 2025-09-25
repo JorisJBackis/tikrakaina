8c6ff7088d60834a9a5c0b1404fe7b2bcf7cd915
JorisJBackis <joris.backis@student.nhlstenden.com>
2025-09-26 02:04:29 +0300

    Fix meta tag syntax in layout.tsx

--- tikrakaina/app/layout.tsx
+++ tikrakaina/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="lt">
       <head> 
-        <meta name="verify-paysera" content="342abafec2abf1abc8d7faa5d63a400a">
+        <meta name="verify-paysera" content="342abafec2abf1abc8d7faa5d63a400a"/>
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
